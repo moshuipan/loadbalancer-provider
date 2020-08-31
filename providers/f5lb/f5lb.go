0beb624bad3a41a4a34a8a844203ecf310cd719a
Huanle Han <hanhuanle@caicloud.io>
2020-08-31 16:19:13 +0800

    fix: should return (#159)

--- providers/f5lb/f5lb.go
+++ providers/f5lb/f5lb.go
@@ -434,6 +434,7 @@ func (p *Provider) onUpdateLBDNS(o *core.QueueObject, tcpCM *v1.ConfigMap) {
 		if err != nil {
 			log.Errorf("Failed to update L7 dns: %v", err)
 		}
+		return
 	}
 
 	// clean All records
